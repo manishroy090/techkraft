fe421450d19ca31986c124b237009c0f5f838950
manishroy090 <89403173+manishroy090@users.noreply.github.com>
2026-07-06 15:50:19 -0400

    Revert "optimized_docker_image_for_nginx_virtual_host_creation"
    This reverts commit 4b21a2eab846aa83004e148296c48db9ea2245a6.

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,10 +6,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 ENV_PATH = BASE_DIR / ".env"
 
 class Settings(BaseSettings):
-    OLLAMA_API_KEY:str = "a976094197904a95ada017c8b2ea12f0.0HKTva-Ay2vbTKELeSepncO1"
-    SECRET_KEY:str = "84caf6135cd07b1fda5dd2067947afde243ad631d1caed9a62e0c329f59cc2b9"
-    ALGORITHM:str ="HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES:int =30
+    OLLAMA_API_KEY:str 
+    SECRET_KEY:str
+    ALGORITHM:str
+    ACCESS_TOKEN_EXPIRE_MINUTES:str
 
     model_config = SettingsConfigDict(env_file=ENV_PATH)
 
